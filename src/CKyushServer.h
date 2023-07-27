5ac585f08ca6173f4c2d8243fa9dfade214a44fd
82207007 <82207007@qq.com>
2023-07-27 21:32:59 +0800

    Add files via upload

---
+++ src/CKyushServer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+class CKyushuServer
+{
+public:
+	CKyushuServer();
+	~CKyushuServer();
+
+public:
+	virtual bool_t			ServerStop() = 0;
+	virtual bool_t			ServerStart() = 0;
+
+protected:
+	void_t					OnAcceptCompletion(HANDLE hConnHandler, const char_t* ip);
+	void_t					OnRecvCompletion(void_t* data, int32_t datalen);
+	void_t					OnCloseCompletion(HANDLE hConnHandler);
+
+public:
+
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+CKyushuServer* InitKyushuServer(int32_t dwServer, int32_t dwDataCenter, int32_t dwWorkThreadCount, int32_t usPort);
